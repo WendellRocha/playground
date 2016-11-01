f5e1d4ce08dd93e4a1e0ab760883da70040f2eb6
Wendell Rocha <wendellrocha@outlook.com>
2016-11-01 09:26:44 -0300

    email python addeded

---
+++ Python/testes-email.py
@@ -0,0 +1,28 @@
+import smtplimport smtplib
+
+'''
+destinatario = 'exemple@exemple.com'
+para = 'exemple@exemple.com'
+header = 'De: '+ destinatario + '\n' + 'Para: ' + para + '\n' + 'Subject: subject \n'
+mensagem = header + '\n mensagem do corpo do email'
+'''
+
+def enviar(email, destinatario, para, mensagem):
+   header = 'De: ' + destinatario + '\n' + 'Para: ' + para + '\n' + 'Subject: teste \n'
+   msg = header + mensagem
+   usuario = 'usuario@exemple.com'
+   senha = 'password'
+   server = smtplib.SMTP('smtp.gmail.com', 587)
+   server.ehlo()
+   server.starttls()
+   server.ehlo()
+   server.login(usuario,senha)
+   server.sendmail(destinatario, para, msg)
+   server.quit()
+   return True
+   
+ cabecalho = 'De: ' + destinatario + '\n' + 'Para: ' + para + '\n' + 'Subject: teste \n'
+ mensagem = '\n hola, que tal?'
+ def teste('wendell.rocha@ccc.ufcg.edu.br', 'wendellrochaa@gmail.com', mensagem):
+	enviar('wendell.rocha@ccc.ufcg.edu.br', 'wendellrochaa@gmail.com', mensagem)
+	print "done!"
