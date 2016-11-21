3d43834f328684d99dd705948acc5e7b9d7c43e9
Wendell Rocha <wendellrocha@outlook.com>
2016-11-20 22:47:53 -0300

    Android application

---
+++ Android/Hello/app/src/main/java/world/hello/HelloWord.java
@@ -0,0 +1,14 @@
+package world.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class HelloWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_word);
+    }
+}
