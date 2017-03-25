e7ebab360264f4e368f95fa128030d9d8b2161a1
Wendell Rocha <wendellrocha@outlook.com>
2017-03-25 03:26:27 -0300

    Powershell scripts

---
+++ PowerShell/backup-command.ps1
@@ -0,0 +1,3 @@
+﻿vmrun -T player stop C:\Users\admin\Desktop\vm\SGA\sga.vmx
+Compress-Archive -Path C:\Users\wende\Documents\"Virtual Machines"\SGA\ -DestinationPath C:\Users\wende\Documents\"Virtual Machines"\backup.zip -Force
+vmrun -T player start C:\Users\admin\Desktop\vm\SGA\sga.vmx
---
+++ PowerShell/backup-script.ps1
@@ -0,0 +1,2 @@
+﻿$backup = New-JobTrigger -Daily -At 1am
+Register-ScheduledJob -Name Backup -ScriptBlock {C:\Users\wende\Documents\GitHub\playground\PowerShell\backup-command.ps1} -Trigger $backup
